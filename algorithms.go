e3e534e15526cae27f6dc7ff5dae3bcde9af8dd4
Asher Shaheen <ashershaheen@gmail.com>
2017-09-08 16:24:13 -0400

    updated algo

--- algorithms.go
+++ algorithms.go
@@ -6,22 +6,29 @@ func main() {
 
 	nums := []int{7, 12, 3, 9, 14, 6, 5, 21, 8, 1}
 
-	sum := 140
-	fmt.Println(nums)
+	//TWO SUMS
+	sum := 14
+
 	num1, num2 := twoSum(nums, sum)
+
 	if num1 == 0 && num2 == 0 && sum != 0 {
 		fmt.Println("None of the numbers added up to", sum)
 	} else {
 		fmt.Println("Two nums that add up to", sum, "are", num1, "and", num2)
 	}
 
+	// CALLBACK FUNCTION FOR MAP ARRAY
 	doubleNum := func(num int) int {
 		num *= 2
 
 		return num
 	}
 
+	// CALL MAPARRAY
 	fmt.Println(mapArray(nums, doubleNum))
+
+	// CALL FACTORIAL
+	fmt.Println(factorial(5))
 
 }
 
@@ -44,4 +51,12 @@ func mapArray(nums []int, f func(num int) int) []int {
 		newArr[i] = f(num)
 	}
 	return newArr
+}
+
+func factorial(num int) int {
+	if num == 0 {
+		return 1
+	}
+
+	return num * factorial(num-1)
 }
